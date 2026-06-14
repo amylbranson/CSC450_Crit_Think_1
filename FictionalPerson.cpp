e2cafb3979e3ad0a1c55b68a708265ce7200395d
amylbranson <amy.branson@csuglobal.edu>
2026-06-14 13:50:19 -0700

    Create FictionalPerson.cpp

---
+++ FictionalPerson.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "First Name: John" << endl;
+    cout << "Last Name: Smith" << endl;
+    cout << "Street Address: 123 Main Street" << endl;
+    cout << "City: Denver" << endl;
+    cout << "Zip Code: 80202" << endl;
+
+    return 0;
+}
